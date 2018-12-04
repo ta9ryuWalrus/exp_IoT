6ab1d37b1d8825bffde48e09bbc0dde1bb971278
ta9ryu <al5.b00taku@gmail.com>
2018-12-04 14:36:48 +0900

    new

---
+++ exp10/tcp_server.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# coding:utf-8
+# tcp_server
+
+import socket
+import threading
+
+bind_ip = '0.0.0.0'
+bind_port = 50420
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_ip, bind_port))
+server.listen(5)
+print(" [*] listen %s:%d" % (bind_ip, bind_port))
+
+def handle_client(client_socket):
+    bufsize = 1024
+    request = client_socket.recv(bufsize)
+    print('[*] recv: %s' % request)
+    client_socket.send("Hey Client!\n")
+    client_socket.close()
+
+while True:
+    client, addr = server.accept()
+    print('[*] connected from: %s:%d' % (addr[0], addr[1]))
+    client_handler = threading.Thread(target = handle_client, args = (client,))
+    client_handler.start()
