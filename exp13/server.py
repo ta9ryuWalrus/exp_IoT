eea7460138e77c8988899503fa0612fa8cafd796
ta9ryu <al5.b00taku@gmail.com>
2018-12-06 16:31:33 +0900

    new

--- exp13/server.py
+++ exp13/server.py
@@ -12,6 +12,7 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip, bind_port))
 server.listen(5)
 print(" [*] listen %s:%d" % (bind_ip, bind_port))
+open("IoT_exercise1_received_data.csv", 'w')
 
 def handle_client(client_socket):
     bufsize = 1024
@@ -20,8 +21,13 @@ def handle_client(client_socket):
         req = request.split(",")
         if len(req) == 4:
             client_socket.send("OK")
+            with open("IoT_exercise1_received_data.csv", 'r') as file:
+                writer = csv.writer(file)
+                writer.writerow(req)
+        else:
+            client_socket.send("ERROR")
 
-        print('[*] recv: %s' % request)
+        #print('[*] recv: %s' % request)
     #client_socket.send("Hey Client!\n")
     #client_socket.close()
 
